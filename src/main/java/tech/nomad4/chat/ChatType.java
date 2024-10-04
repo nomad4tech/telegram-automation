3c9d4b0680d07cc8e8d653ed346da67ca0d23bc1
nomad4tech <alex.sav4387@gmail.com>
2024-10-05 01:28:11 +0300

    add documentation for enum ChatType

--- src/main/java/tech/nomad4/chat/ChatType.java
+++ src/main/java/tech/nomad4/chat/ChatType.java
@@ -1,5 +1,15 @@
 package tech.nomad4.chat;
 
+/**
+ * Represents the type of Telegram chat.
+ * <p>
+ * This enumeration defines two types of chats:
+ * </p>
+ * <ul>
+ *     <li>{@link #PRIVATE} - Represents private chat with individual user.</li>
+ *     <li>{@link #GROUP} - Represents group chat that can contain multiple participants.</li>
+ * </ul>
+ */
 public enum ChatType {
     PRIVATE,
     GROUP
